f7518a1739a64b6bfb26b3057164c69f88bf5768
bilalsadiq03 <bnaq860@gamil.com>
2024-06-08 09:51:22 +0530

    Update Category routes & controller

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,17 +1,13 @@
 // Controller for creating the category
 const category_model = require("../models/category.model")
+const mongoose = require("mongoose")
 
+exports.createNewCategory = async (req, res)=>{
 
-exports.createNewCtegory = async (req, res)=>{
-    // Read the category body
-
-    // Create the category object
     const categoryData = {
         name: req.body.name,
         description: req.body.description 
     } 
-
-    // Insert into mongodb
     try {
         const category = await category_model.create(categoryData)
         res.status(201).send(category)
@@ -20,12 +16,70 @@ exports.createNewCtegory = async (req, res)=>{
         return res.status(500).send({
             message: "Error while creating the category"
         })
-        
     }
-
-    // Return the response of the craeted category
 }
 
+exports.updateCategory  =async (req, res) => {
+    try {
+        const { name } = req.body;
+        const { categoryId } = req.params;
+    
+        const category = await category_model.findOne({ _id: categoryId });
+    
+        if (!category) {
+          return res.status(404).send({ 
+            error: "Category not found" 
+        });
+        }
+    
+        category.name = name;
+    
+        const updatedCategory = await category.save();
+        res.send(updatedCategory);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ 
+            error: "Internal server error" 
+        });
+      }
+}
+
+exports.removeCategory = async (req, res) => {
+    try {
+        const removed = await category_model.findByIdAndDelete(req.params._id);
+        res.status(200).send({
+            message: "Category removed successfully!"
+        });
+
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal server error" });
+      }
+} 
+
+exports.listCategory = async (req, res) => {
+    try {
+        const all = await category_model.find();
+        res.status(200).send(all);
+      } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message);
+      }
+}
+
+exports.readCategory = async (req, res) => {
+   
+    try {
+        const category = await category_model.findOne({_id: req.params.id});
+        if (!category) {
+            return res.status(404).send({ message: 'Category not found' });
+        }
+        res.status(200).send({category});
+      } catch (error) {
+        console.log(error);
+        return res.status(400).send(error.message);
+      }
+}
 
 
 
--- routes/category.route.js
+++ routes/category.route.js
@@ -4,5 +4,13 @@ category_controller = require("../controllers/category.controller")
 auth_mw = require("../middlewares/auth.mw.js")
 
 module.exports = (app)=> {
-    app.post("/ecomm/api/v1/auth/categories", [auth_mw.verifyToken, auth_mw.isAdmin],category_controller.createNewCtegory)
+    app.post("/v1/auth/categories", [auth_mw.verifyToken, auth_mw.isAdmin],category_controller.createNewCategory),
+
+    app.put("/v1/auth/categories/:categoryId", [auth_mw.verifyToken, auth_mw.isAdmin], category_controller.updateCategory),
+
+    app.delete("/v1/auth/categories/:categoryId", [auth_mw.verifyToken, auth_mw.isAdmin], category_controller.removeCategory)
+
+    app.get("/v1/auth/categories/all",[auth_mw.verifyToken], category_controller.listCategory),
+
+    app.get("/v1/auth/categories/:id", category_controller.readCategory)
 }
